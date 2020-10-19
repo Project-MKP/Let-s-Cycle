e4c7336f77d59fb016b9a903bbb113e2cb5f551c
PI3R23 <ppitrowski2@gmail.com>
2020-10-19 12:28:16 +0200

    interface structure wth lbl, lstviw

--- letscycle/letscycle/obj/Debug/netstandard2.0/Page2.xaml.g.cs
+++ letscycle/letscycle/obj/Debug/netstandard2.0/Page2.xaml.g.cs
@@ -17,8 +17,12 @@ namespace letscycle {
     public partial class Page2 : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ListView chart;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Page2));
+            chart = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "chart");
         }
     }
 }
